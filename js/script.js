95871cde8d3fd8e88b19b45bde17f85b2f761b73
Michael <michael.philipe2013@gmail.com>
2023-03-30 17:25:52 -0300

    modal created

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const container = document.getElementById('card-user-container');
 const url = 'https://randomuser.me/api/?results=3';
 const plusIcon = document.getElementById('find-users');
+// let currentAuthor = [];
+
+//converter para arrow function
 
 function createNode(element) {
   return document.createElement(element);
@@ -15,7 +18,12 @@ function getDataFromAPI(url) {
     .then((response) => response.json())
     .then((data) => {
       clearCards();
-      populatePage(data.results);
+      DisplayCards(data.results);
+      const buttonViewProfile = document.querySelectorAll('.trigger');
+
+      buttonViewProfile.forEach((currentButton) =>
+        currentButton.addEventListener('click', switchModal)
+      );
     })
     .catch((error) => console.error(error));
 }
@@ -24,7 +32,7 @@ function clearCards() {
   container.innerHTML = '';
 }
 
-function populatePage(authors) {
+function DisplayCards(authors) {
   console.log(authors);
   authors.map(function (author) {
     let div = createNode('div');
@@ -38,14 +46,17 @@ function populatePage(authors) {
     div.classList.add('card', 'card-user');
 
     icon.classList.add('icon');
+    button.classList.add('trigger');
     author.gender === 'female'
       ? (icon.src = './img/venus-solid.svg')
       : (icon.src = './img/mars-solid.svg');
     img.src = author.picture.large;
     name.innerHTML = `${author.name.first} ${author.name.last}`;
     birthDate.innerHTML = '01/01/2002';
-    phone.innerHTML = author.phone;
+    phone.innerHTML = phoneMask(author.phone);
     button.innerHTML = 'View Profile';
+    div.addEventListener('click', () => populateModal(author));
+
     append(container, div);
     append(div, icon);
     append(div, img);
@@ -56,8 +67,60 @@ function populatePage(authors) {
   });
 }
 
+const switchModal = (event) => {
+  const modal = document.querySelector('.modal');
+  const actualStyle = modal.style.display;
+  if (actualStyle === 'block') {
+    modal.style.display = 'none';
+  } else {
+    modal.style.display = 'block';
+  }
+};
+
+const phoneMask = (value) => {
+  //não vai funcionar certinho para todos os valores pois vao ser de diferentes localizaçoes, ficaria bom com numeros brasileiros
+  if (!value) return '';
+
+  value = value.replace(/\D/g, '');
+  value = value.replace(/(\d{2})(\d)/, '($1) $2');
+  value = value.replace(/(\d)(\d{4})$/, '$1-$2');
+  return value;
+};
+
+const populateModal = (person) => {
+  console.log(person);
+  const avatar = document.getElementById('avatar');
+  const h3 = document.querySelector('.avatar-name');
+  const spanLocation = document.querySelector('.location');
+  const spanEmail = document.querySelector('.email');
+  const spanPhone = document.querySelector('.phone');
+  const spanAge = document.querySelector('.age');
+  const spanAddress = document.querySelector('.address');
+  const spanUsername = document.querySelector('.username');
+
+  avatar.src = `${person.picture.large}`;
+  h3.innerHTML = `${person.name.first} ${person.name.last}`;
+  spanLocation.innerHTML = `${person.location.state}, ${person.location.country}`;
+  spanEmail.innerHTML = person.email;
+  spanPhone.innerHTML = phoneMask(person.phone);
+  spanAge.innerHTML = `${person.dob.age} years`;
+  spanAddress.innerHTML = `${person.location.street.name}, ${person.location.street.number}`;
+  spanUsername.innerHTML = person.login.username;
+
+  //montar o modal
+};
+
 plusIcon.addEventListener('click', () => {
   getDataFromAPI(url);
 });
 
 getDataFromAPI(url);
+
+// name, location, country, email, age, phone , picture username?
+
+window.onclick = (e) => {
+  const modal = document.querySelector('.modal');
+  if (e.target === modal) {
+    switchModal();
+  }
+};
